8148b5a448f239bc11e8f01baefe8e163d984119
George R <riordan.gr@gmail.com>
2016-05-20 03:09:56 +1200

    Add backend for main menu buttons

---
+++ app/src/main/java/com/vuw/project1/riverwatch/activities/HistoryActivity.java
@@ -0,0 +1,15 @@
+package com.vuw.project1.riverwatch.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.vuw.project1.riverwatch.R;
+
+public class HistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+    }
+}
--- app/src/main/java/com/vuw/project1/riverwatch/activities/MainActivity.java
+++ app/src/main/java/com/vuw/project1/riverwatch/activities/MainActivity.java
@@ -1,16 +1,61 @@
 package com.vuw.project1.riverwatch.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.vuw.project1.riverwatch.R;
 
 public class MainActivity extends AppCompatActivity {
+    private Button reportButton;
+    private Button nitrateButton;
+    private Button bluetoothButton;
+    private Button historyButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Cool
+
+        reportButton = (Button)findViewById(R.id.reportButton);
+        nitrateButton = (Button)findViewById(R.id.nitrateButton);
+        bluetoothButton = (Button)findViewById(R.id.bluetoothButton);
+        historyButton = (Button)findViewById(R.id.historyButton);
+
+
+        reportButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ReportActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        nitrateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NitrateActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        bluetoothButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, BluetoothActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        historyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HistoryActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
