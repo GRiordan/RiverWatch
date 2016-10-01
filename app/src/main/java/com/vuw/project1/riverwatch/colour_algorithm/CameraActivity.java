e6135cf6ea87cbd2e54d06f5d2c481511aa74d6d
George R <riordan.gr@gmail.com>
2016-10-01 15:52:25 +1300

    fix import issue

--- app/src/main/java/com/vuw/project1/riverwatch/colour_algorithm/CameraActivity.java
+++ app/src/main/java/com/vuw/project1/riverwatch/colour_algorithm/CameraActivity.java
@@ -14,6 +14,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Display;
+import android.view.MotionEvent;
 import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
